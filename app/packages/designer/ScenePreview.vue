f92b087206fd2ae0e11297abc7e72eacccf02071
liuhan <liuhann@gmail.com>
2019-01-23 10:43:52 +0800

    Add enter and leave effect

--- app/packages/designer/ScenePreview.vue
+++ app/packages/designer/ScenePreview.vue
@@ -46,7 +46,7 @@ export default {
     'currentElement': {
       deep: true,
       handler () {
-        console.log('current element change', this.currentElement.id, this.currentElement)
+        console.log('current element change', this.currentElement.id, this.currentElement, this.scene)
         this.renderElement(this.currentElement)
       }
     },
