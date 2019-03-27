342cd9d73b8357dec135fe82e391cfa3317da6da
liuhan <liuhann@gmail.com>
2019-03-27 17:36:58 +0800

    Add enter and leave effect

---
+++ packages/anim-maker/maker.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="animation-maker">
+
+</div>
+</template>
+
+<script>
+  export default {
+    name: 'maker',
+    data () {
+      return {
+        timelines: [{
+          name: 'start'
+        }, {
+          name: 'end'
+        }]
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
