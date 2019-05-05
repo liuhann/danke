376d3e10a59a73453dc05e123e87aa4aeab5821f
15011245191 <liuhann@gmail.com>
2019-05-06 07:00:38 +0800

    12

--- app-story/main.js
+++ app-story/main.js
@@ -1,20 +1,8 @@
 import './common.scss'
-import '../packages/webfont/karla/font.css'
-import '../packages/webfont/fontello/css/fontello.css'
 import AsyncBoot from 'async-boot'
 import App from './app.vue'
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
-// import home from '../packages/home'
-import site from '../packages/site'
-import designer from '../packages/designer'
-import runtime from '../packages/runtime'
-import user from '../packages/user'
-import frameTool from '../packages/frame-tool'
-import styleTool from '../packages/style-tool'
-
-import initClient from './middlewares/initClient'
 
 Vue.use(VueRouter)
 window.Vue = Vue
@@ -28,15 +16,9 @@ const boot = new AsyncBoot({
   cdn: 'http://cdn.danke.fun',
   mount: '#app',
   packages: [
-    site, designer, runtime, user, frameTool, styleTool
   ],
   started: async (ctx, next) => {
-    initClient(ctx)
-    ctx.user = await ctx.userdao.getCurrentUser()
     await next()
-  },
-  upload: {
-    maxSize: 2 * 1024 * 1024
   }
 })
 boot.startUp()
