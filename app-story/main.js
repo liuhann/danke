dedf02a718351c4b0e9967a33e0fca035da73ebe
15011245191 <liuhann@gmail.com>
2019-05-06 06:50:19 +0800

    add story npm

---
+++ app-story/main.js
@@ -0,0 +1,42 @@
+import './common.scss'
+import '../packages/webfont/karla/font.css'
+import '../packages/webfont/fontello/css/fontello.css'
+import AsyncBoot from 'async-boot'
+import App from './app.vue'
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+// import home from '../packages/home'
+import site from '../packages/site'
+import designer from '../packages/designer'
+import runtime from '../packages/runtime'
+import user from '../packages/user'
+import frameTool from '../packages/frame-tool'
+import styleTool from '../packages/style-tool'
+
+import initClient from './middlewares/initClient'
+
+Vue.use(VueRouter)
+window.Vue = Vue
+
+const boot = new AsyncBoot({
+  Vue,
+  VueRouter,
+  App,
+  mode: 'history',
+  appServer: 'http://www.danke.fun',
+  cdn: 'http://cdn.danke.fun',
+  mount: '#app',
+  packages: [
+    site, designer, runtime, user, frameTool, styleTool
+  ],
+  started: async (ctx, next) => {
+    initClient(ctx)
+    ctx.user = await ctx.userdao.getCurrentUser()
+    await next()
+  },
+  upload: {
+    maxSize: 2 * 1024 * 1024
+  }
+})
+boot.startUp()
---
+++ packages/common/initClient.js
@@ -0,0 +1,30 @@
+import ky from 'ky'
+function randomRangeId (num) {
+  let returnStr = ''
+  const charStr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  for (let i = 0; i < num; i++) {
+    let index = Math.round(Math.random() * (charStr.length - 1))
+    returnStr += charStr.substring(index, index + 1)
+  }
+  return returnStr
+}
+
+export default function initClient (ctx) {
+  let token = window.localStorage.getItem('token')
+  if (!token) {
+    token = randomRangeId(10)
+    window.localStorage.setItem('token', token)
+  }
+  const client = ky.extend({
+    prefixUrl: 'http://www.danke.fun/api/',
+    throwHttpErrors: false,
+    searchParams: {
+      'token': token
+    }
+  })
+  ctx.ky = client
+  ctx.get = client.get
+  ctx.put = client.put
+  ctx.post = client.post
+  ctx.delete = client.delete
+}
