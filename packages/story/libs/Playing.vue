bf3907cfde5c10b6e3c881698f32abba8efa7d56
15011245191 <liuhann@gmail.com>
2019-05-08 07:08:34 +0800

    11212

---
+++ packages/story/libs/Playing.vue
@@ -0,0 +1,30 @@
+<template>
+<div class="modal player">
+  <div class="modal-background"></div>
+  <div class="modal-content">
+      
+  </div>
+  <button class="modal-close is-large" aria-label="close"></button>
+</div>
+
+</template>
+
+<script>
+export default {
+  name: 'Playing.vue',
+  components: {  },
+  data () {
+    return {
+
+    }
+  },
+  created () {
+  },
+  methods: {
+    
+  }
+}
+</script>
+<style lang="sass">
+$body-background-color: #0B2239
+</style>
