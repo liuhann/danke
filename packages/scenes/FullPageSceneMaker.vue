15073473494ff0f3d9c54061bde1c7b3686a2034
liuhan <liuhann@gmail.com>
2019-06-19 17:36:07 +0800

    framing box

--- packages/scenes/FullPageSceneMaker.vue
+++ packages/scenes/FullPageSceneMaker.vue
@@ -1,124 +1,171 @@
 <template>
-<div id="scene-maker">
-	<div class="tool-bar"></div>
-	<div class="sider-bar"></div>
-
-	<div class="scene-container" ref="container">
-		<div class="device-drag"></div>
-		<div class="device draggable-source" ref="device">
-			<div class="element"></div>
-		</div>
-	</div>
-	<div class="line"></div>
-</div>
+  <div id="scene-maker">
+    <div class="tool-bar">
+      <a class="button is-white is-small">White</a>
+      <a class="button is-white is-small" @click="zoomIn">
+        <i class="icon-plus"></i>
+      </a>
+      {{zoom}}
+    </div>
+    <div class="sider-bar"></div>
+    <div class="scene-container" ref="container">
+      <div class="device draggable-source" ref="device" :style="deviceStyle">
+        <div class="ti"></div>
+      </div>
+    </div>
+    <div class="line"></div>
+  </div>
 </template>
 
 <script>
-import { Draggable } from '@shopify/draggable'
 import interact from 'interactjs'
-
+import { fitToContainer } from '../danke-core/utils/common'
 export default {
-	name: 'FullPageSceneMaker',
-	mounted () {
-    interact('.device')
+  name: 'FullPageSceneMaker',
+  props: {
+    ratio: {
+      type: String,
+      default: '9:16'
+    }
+  },
+  data () {
+    return {
+      device: {
+        width: 360,
+        height: 640
+      },
+      zoom: 1,
+      position: {
+        left: 0,
+        top: 0
+      }
+    }
+  },
+  computed: {
+    deviceStyle () {
+      return {
+        width: this.device.width * this.zoom + 'px',
+        height: this.device.height * this.zoom + 'px'
+      }
+    }
+  },
+  mounted () {
+    this.autoZoomFitContainer()
+    /*interact('.ti')
       .draggable({
-        // enable inertial throwing
-        inertia: true,
         // keep the element within the area of it's parent
-        modifiers: [
-          interact.modifiers.restrict({
-            restriction: "parent",
-            endOnly: true,
-            elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
-          }),
-        ],
-        // enable autoScroll
-        autoScroll: true,
-
+        // modifiers: [
+        //   interact.modifiers.restrict({
+        //     restriction: 'parent',
+        //     endOnly: true,
+        //     elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+        //   })
+        // ],
         // call this function on every dragmove event
         onmove: this.dragMoveListener,
         // call this function on every dragend event
         onend: function (event) {
-          var textEl = event.target.querySelector('p');
-
-          textEl && (textEl.textContent =
-            'moved a distance of '
-            + (Math.sqrt(Math.pow(event.pageX - event.x0, 2) +
-            Math.pow(event.pageY - event.y0, 2) | 0))
-              .toFixed(2) + 'px');
         }
-      });
-	},
-	methods: {
+      })*/
+    interact('.ti').draggable({
+      onmove: this.dragMoveListener
+    })
+  },
+  methods: {
+    zoomIn () {
+      this.zoom = Math.floor((this.zoom - 0.1) * 10) / 10
+    },
+    zoomOut () {
+      this.zoom = Math.floor((this.zoom + 0.1) * 10) / 10
+    },
+    autoZoomFitContainer () {
+      const containerEl = this.$refs.container
+      this.device = fitToContainer(this.ratio, containerEl.clientWidth, containerEl.clientHeight - 40)
+    },
     dragMoveListener (event) {
-  var target = event.target,
-    // keep the dragged position in the data-x/data-y attributes
-    x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
-    y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+      let target = event.target
+      console.log(target)
+      target = target.parentElement
+      const x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
+      const y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
+      // translate the element
+      target.style.webkitTransform =
+        target.style.transform =
+          'translate(' + x + 'px, ' + y + 'px)'
 
-  // translate the element
-  target.style.webkitTransform =
-    target.style.transform =
-      'translate(' + x + 'px, ' + y + 'px)';
-
-  // update the posiion attributes
-  target.setAttribute('data-x', x);
-  target.setAttribute('data-y', y);
-}
-	}
+      // update the posiion attributes
+      target.setAttribute('data-x', x)
+      target.setAttribute('data-y', y)
+    }
+  }
 }
 </script>
 
 <style lang="scss">
-html.has-navbar-fixed-top, body.has-navbar-fixed-top {
-	padding-top: 0;
-}
+  html.has-navbar-fixed-top, body.has-navbar-fixed-top {
+    padding-top: 0;
+  }
 
-#scene-maker {
-	position: relative;
-	left: 0;
-	top: 0;
-	width: 100%;
-	height: 100%;
-	overflow: hidden;
-	background-color: #f5f5f4;
-	.scene-container {
-		position: absolute;
-		left: 0;
-		top: 36px;
-		bottom: 0;
-		right: 0;
-		.device-drag {
-			position: absolute;
-			top: 0;
-			left: 0;
-			bottom: 0;
-			right: 0;
-			z-index: 1;
-		}
-		.device {
-			position: absolute;
-			left: 20px;
-			background-color: #fff;
-			width: 200px;
-			height: 299px;
-			z-index: 100;
-			.element {
-				position: absolute;
-				background-color: #0a0a0a;
-				width: 30px;
-				height: 30px;
-			}
-		}
-	}
-}
-.line {
-	margin:5px 0;
-	height:	4px;
-	background:
-		repeating-linear-gradient(to right,red 0,red 5px,transparent 5px,transparent 7px);
-	/*5px red then 2px transparent -> repeat this!*/
-	background-size: 100% 1px;
-	background-repeat: no-repeat;
-}
+  #scene-maker {
+    position: relative;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    background-color: #f5f5f4;
+    .tool-bar {
+      font-size: 0.75rem;
+      height: 2.25em;
+      line-height: 2.25em;
+      background-color: #fff;
+    }
+    .scene-container {
+      position: absolute;
+      left: 0;
+      font-size: 0.75rem;
+      top: 2.5em;
+      bottom: 0;
+      right: 0;
+
+      .device-drag {
+        position: absolute;
+        top: 0;
+        left: 0;
+        bottom: 0;
+        right: 0;
+        z-index: 1;
+      }
+
+      .device {
+        touch-action: none;
+        position: absolute;
+        left: 20px;
+        background-color: #fff;
+        z-index: 100;
+        .element {
+          position: absolute;
+          background-color: #0a0a0a;
+          width: 30px;
+          height: 30px;
+        }
+        .ti {
+          position: absolute;
+          background-color: #0a0a0a;
+          left: -30px;
+          width: 30px;
+          height: 30px;
+        }
+      }
+    }
+  }
+
+  .line {
+    margin: 5px 0;
+    height: 4px;
+    background: repeating-linear-gradient(to right, red 0, red 5px, transparent 5px, transparent 7px);
+    /*5px red then 2px transparent -> repeat this!*/
+    background-size: 100% 1px;
+    background-repeat: no-repeat;
+  }
 </style>
