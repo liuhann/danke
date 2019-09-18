f415143b13462deea4a48415ced6180dbc92f34c
15011245191 <liuhann@gmail.com>
2019-09-19 07:39:40 +0800

    ddd

--- packages/xd-builder/components/ElementConfig.vue
+++ packages/xd-builder/components/ElementConfig.vue
@@ -1,25 +1,26 @@
 <template>
 <nav class="element-prop-config">
-  <div class="form-title">
+  <!-- <div class="form-title">
     <div class="text">元素配置</div>
     <div class="opers">
       <span class="icon-clone" @click="copyElement"></span>
       <span class="icon-scissors" @click="cutElement"></span>
       <span class="icon-paste" :class="[clipboard.elements.length > 0? 'has-clipboard': '']" @click="pasteElement"></span>
     </div>
-  </div>
+  </div> -->
   <edit-font v-if="element.font" v-model="element.font"></edit-font>
   <edit-background v-model="element.background" v-if="element.background"></edit-background>
   <edit-position v-model="element.position"></edit-position>
   <edit-size v-model="element.size"></edit-size>
-  <edit-mask v-model="element.mask" v-if="element.mask"></edit-mask>
+  <edit-border v-model="element.border" v-if="element.border"></edit-border>
+  <edit-shadow v-model="element.shadow" v-if="element.shadow"></edit-shadow>
+
   <tabs size="is-small" v-model="animationType" :tabs="animationTabs"></tabs>
   <edit-animation v-if="animationType==='in'" :animation="element.animation.in" animation-type="in"></edit-animation>
   <edit-animation v-if="animationType==='dura'" :animation="element.animation.dura" animation-type="dura"></edit-animation>
   <edit-animation v-if="animationType==='out'" :animation="element.animation.out" animation-type="out"class="bottom-line"></edit-animation>
 
-  <edit-border v-model="element.border" v-if="element.border"></edit-border>
-  <edit-shadow v-model="element.shadow" v-if="element.shadow"></edit-shadow>
+  <edit-mask v-model="element.mask" v-if="element.mask"></edit-mask>
   <edit-clip-path v-model="element.clip" v-if="element.clip"></edit-clip-path>
 
   <div class="buttons has-addons">
