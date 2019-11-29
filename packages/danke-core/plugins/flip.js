11ebc077c8d7e1e0f240b5e56d4bccbfc0543231
liuhan <liuhann@gmail.com>
2019-11-29 12:56:31 +0800

    add basic flip control

---
+++ packages/danke-core/plugins/flip.js
@@ -0,0 +1,15 @@
+import POSITION from '../css-model/position'
+import SIZE from '../css-model/size'
+const Flip = {
+  version: '1.2',
+  type: 12,
+  name: '',
+  position: POSITION,
+  size: SIZE,
+  front: '',
+  back: '',
+  speed: 1000,
+  delay: 1000,
+  rotate: 'x'
+}
+export default Flip
