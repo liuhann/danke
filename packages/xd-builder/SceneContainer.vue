649279f887a6e5478675f914bb9907cd86237e91
15011245191 <liuhann@gmail.com>
2020-02-01 19:45:42 +0800

    make drag area visible

--- packages/xd-builder/SceneContainer.vue
+++ packages/xd-builder/SceneContainer.vue
@@ -1,16 +1,14 @@
 <template>
 <section id="scene-container">
-  <div class="screen">
-    <div class="scene" @dragover="sceneDragOver" v-if="scene" :style="scene.style" @click.self="sceneClick">
+  <div class="screen" :style="styleScreen">
+    <div class="scene" @dragover="sceneDragOver" v-if="scene">
       <render-element
           v-for="(element, index) of scene.elements"
           stage="in"
           :element="element"
           :key="element.id"
           :index="index"
-          :ref="element.id"
-          :selected="currentElement === element"
-          @click="chooseElement(element, $event)"/>
+          :ref="element.id"/>
     </div>
   </div>
   <div class="mask">
@@ -38,10 +36,19 @@ export default {
         x: 0,
         y: 0
       },
+      // 屏幕区的位置
+      screenRect: {
+        x: 0,
+        y: 0,
+        width: 0,
+        height: 0
+      },
       // 拖拽区域信息
       dragRect: {
+        // 坐标 x, y
         left: 0,
         top: 0,
+        // 宽度高度
         width: 0,
         height: 0,
         visible: false
@@ -50,6 +57,14 @@ export default {
   },
 
   computed: {
+    styleScreen () {
+      return {
+        left: this.screenRect.x + 'px',
+        top: this.screenRect.y + 'px',
+        width: this.screenRect.width + 'px',
+        height: this.screenRect.height + 'px'
+      }
+    },
     // 拖拽中的矩形样式
     styleDragingRect () {
       return {
@@ -73,11 +88,18 @@ export default {
           this.dragRect.left = event.x0 - event.rect.left
           this.dragRect.top = event.y0 - event.rect.top
           this.dragRect.visible = true
-          console.log('drag start', event)
         },
         onmove: event => {
-          this.dragRect.width = event.page.x - event.rect.left
-          this.dragRect.height = event.page.y - event.rect.top
+          this.dragRect.width = event.page.x - event.x0
+          this.dragRect.height = event.page.y - event.y0
+          if (this.dragRect.width < 0) {
+            this.dragRect.left = (event.x0 - event.rect.left) + this.dragRect.width
+            this.dragRect.width = -this.dragRect.width
+          }
+          if (this.dragRect.height < 0) {
+            this.dragRect.top = (event.y0 - event.rect.top) + this.dragRect.height
+            this.dragRect.height = -this.dragRect.height
+          }
           console.log('drag move', event)
         },
         onend: event => {
@@ -95,6 +117,10 @@ export default {
     },
 
     fitToCenter () {
+      this.screenRect.width = this.screen.width
+      this.screenRect.height = this.screen.height
+      this.screenRect.x = (this.$el.clientWidth - this.screen.width) / 2
+      this.screenRect.y = 20
       console.log(this.$el.clientWidth, this.$el.clientHeight)
     }
   }
@@ -107,6 +133,11 @@ export default {
   touch-action: none;
   user-select: none;
   flex: 1;
+  .screen {
+    position: absolute;
+    background-image: url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAwAAAAMCAIAAADZF8uwAAAAGUlEQVQYV2M4gwH+YwCGIasIUwhT25BVBADtzYNYrHvv4gAAAABJRU5ErkJggg==");
+    box-shadow: 0 0 6px #ddd;
+  }
   .mask {
     .dragging-rect {
       position: absolute;
